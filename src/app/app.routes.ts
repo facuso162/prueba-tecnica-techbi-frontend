e0c8d6011fea7e45891de310919bfa96fca90342
facuso162 <facu.sosa.gabriel@gmail.com>
2025-09-14 20:14:07 -0300

    rutas home y creacion de ensayo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { EnsayoForm } from './pages/ensayo-form/ensayo-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'ensayo/nuevo', component: EnsayoForm },
+];
