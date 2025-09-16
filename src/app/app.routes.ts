cc3f4a1bc3bd77e4210b779040381aa7f154ff61
facuso162 <facu.sosa.gabriel@gmail.com>
2025-09-16 16:18:23 -0300

    ruta de edicion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { EnsayoForm } from './pages/ensayo-form/ensayo-form';
+import { EnsayoEditForm } from './pages/ensayo-edit-form/ensayo-edit-form';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'ensayo/nuevo', component: EnsayoForm },
+  { path: 'ensayo/:codigo', component: EnsayoEditForm },
 ];
