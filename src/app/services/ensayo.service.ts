830362ff9424f394ed4eb027a08836266d014a3e
facuso162 <facu.sosa.gabriel@gmail.com>
2025-09-16 16:17:50 -0300

    implementacion completa del servicio de ensayos

--- src/app/services/ensayo.service.ts
+++ src/app/services/ensayo.service.ts
@@ -16,7 +16,7 @@ export type Ensayo = {
   }[];
 };
 
-type ApiResponse = { success: true; data: Ensayo[] } | { success: false; message: string };
+type ApiResponse = { success: true; data: any } | { success: false; message: string };
 
 @Injectable({ providedIn: 'root' })
 export class EnsayoService {
@@ -24,6 +24,7 @@ export class EnsayoService {
   private apiUrl = 'http://localhost:3000/ensayos';
 
   getAll(): Observable<Ensayo[]> {
+    // Aca data es un array de ensayos
     return this.http.get<ApiResponse>(this.apiUrl).pipe(
       map((res) => {
         if ('success' in res) {
@@ -37,11 +38,74 @@ export class EnsayoService {
     );
   }
 
-  // getById() - todo
+  getById(codigo: number): Observable<Ensayo> {
+    return this.http.get<ApiResponse>(`${this.apiUrl}/${codigo}`).pipe(
+      map((res) => {
+        if ('success' in res) {
+          if (res.success) {
+            return res.data;
+          }
+          throw new Error(res.message);
+        }
+        throw new Error('Error desconocido');
+      })
+    );
+  }
 
-  // create() - todo
+  create(nuevoEnsayo: {
+    nombre: string;
+    descripcion: string;
+    formula: string;
+    pruebas: { descripcion: string; valor: number }[];
+  }): Observable<Ensayo> {
+    // Aca data es un objeto con la estructura del nuevo ensayo
+    return this.http.post<ApiResponse>(this.apiUrl, nuevoEnsayo).pipe(
+      map((res) => {
+        if ('success' in res) {
+          if (res.success) {
+            return res.data;
+          }
+          throw new Error(res.message);
+        }
+        throw new Error('Error desconocido');
+      })
+    );
+  }
 
-  // update() - todo
+  delete(codigo: number): Observable<void> {
+    return this.http.delete<ApiResponse>(`${this.apiUrl}/${codigo}`).pipe(
+      map((res) => {
+        if ('success' in res) {
+          if (res.success) {
+            return;
+          }
+          throw new Error(res.message);
+        }
+        throw new Error('Error desconocido');
+      })
+    );
+  }
 
-  // delete() - todo
+  update(
+    codigo: number,
+    data: {
+      nombre?: string;
+      descripcion?: string;
+      formula?: string;
+      pruebasACrear?: { descripcion: string; valor: number }[];
+      pruebasAEliminar?: number[];
+    }
+  ): Observable<Ensayo> {
+    return this.http.put<ApiResponse>(`${this.apiUrl}/${codigo}`, data).pipe(
+      map((res) => {
+        if ('success' in res) {
+          if (res.success) {
+            return res.data;
+          }
+          throw new Error(res.message);
+        }
+        throw new Error('Error desconocido');
+      })
+    );
+  }
 }
